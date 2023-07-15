ba1bce481e956d05def22c0379ddcf59ff2aaf40
VladBogda <vladbogdantsev@gmail.com>
2023-07-15 18:34:29 +0300

    function1 (ch1-5 done)

---
+++ task-tdd-10/src/main/java/vladproduction/function1/Main01.java
@@ -0,0 +1,56 @@
+package vladproduction.function1;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class Main01 {
+
+    private final double EPS = 1e-6;
+    private final double a = -0.5;
+    private final double b = 2.0;
+    public double function(double x) {
+
+        if(x<=0.7+EPS)return 1; //need to add EPS, so test could run with calculation error
+        if(x>1.4+EPS)return Math.exp(a*x)*Math.cos(b*x);
+        return a*x*x*Math.log(x);
+    }
+
+    public int stepsCount(double start, double finish, double step) {
+        return (int)(Math.round((finish-start)/step) + 1);
+    }
+
+    public double[] writeX(double start, double finish, double step) {
+        return IntStream.range(0,stepsCount(start, finish, step)).
+                         mapToDouble(i-> start + i * step).
+                         toArray();
+    }
+
+    public double[] writeY(double[] x) {
+        return DoubleStream.of(x).map(this::function).toArray();
+    }
+
+    public int findIndexMaxValue(double[] y) {
+        int res = 0;
+        for (int i = 0; i < y.length; i++) {
+            double max = y[i];
+            int maxIndex = i;
+            for (int j = i+1; j < y.length;j++){
+                if(y[j]>max){
+                    maxIndex = j;
+                    max = y[j];
+                }
+            }
+            if(maxIndex != i){
+                swap(y, i,maxIndex);
+                res = maxIndex;
+            }
+        }
+        return res;
+    }
+
+    private static void swap(double[] data, int i, int j) {
+        data[i] = data[i] + data[j];
+        data[j] = data[i] - data[j];
+        data[i] = data[i] - data[j];
+    }
+}
---
+++ task-tdd-10/src/test/java/vladproduction/function1/StaticDataProvider01.java
@@ -0,0 +1,49 @@
+package vladproduction.function1;
+
+import org.testng.annotations.DataProvider;
+
+public class StaticDataProvider01 {
+
+    @DataProvider(name = "dataForFunction")
+    public static Object[][] createDataForFunction() {
+        return new Object[][]{
+                {0.5, 1},
+                {2.0, -0.240462},
+                {1.0, 0.0}, //not good example, test working(!) without any logic in function
+                {1.1, -0.057662}
+        };
+    }
+
+    @DataProvider(name = "dataForSteps")
+    public static Object[][] createDataForSteps() {
+        return new Object[][]{
+                {0.0, 10.0, 0.1, 101},
+                {0.0, 3.0, 0.004, 751}
+        };
+    }
+
+    @DataProvider(name = "dataForWriteX")
+    public static Object[][] createDataForWriteX() {
+        return new Object[][]{
+                {175, 0.7},
+                {350, 1.4},
+                {750, 3.0}
+        };
+    }
+
+    @DataProvider(name = "dataForWriteY")
+    public static Object[][] createDataForWriteY() {
+        return new Object[][]{
+                {175, 1.0},
+                {350, -0.329742}, //(example how to count): -0.5*(1.4^2)*ln(1.4)
+                {750, 0.214242} //Math.exp(a*x)*Math.cos(b*x);--> e^(-1.5)*cos(6)-->e^(-0.5*3)cos(2*3)
+        };
+    }
+
+    @DataProvider(name = "findIndexMax")
+    public static Object[][] findIndexMax() {
+        return new Object[][]{
+
+        };
+    }
+}
