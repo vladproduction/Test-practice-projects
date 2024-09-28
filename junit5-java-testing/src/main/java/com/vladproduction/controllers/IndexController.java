e0e6fe83a0ee4c78c90c16a135a303e96f555f47
vladproduction <vladbogdantsev@gmail.com>
2024-09-28 21:41:07 +0300

    junit5-java-testing (project add); start position

---
+++ junit5-java-testing/src/main/java/com/vladproduction/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.vladproduction.controllers;
+
+public class IndexController {
+
+    public String index(){
+
+        return "index";
+    }
+
+    public String oupsHandler(){
+        return "notimplemented";
+    }
+}
