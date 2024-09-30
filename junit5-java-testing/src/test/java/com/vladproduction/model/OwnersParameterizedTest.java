bcab5830663f46f6470be4d3f26a9c6dca9b5875
vladproduction <vladbogdantsev@gmail.com>
2024-09-30 18:55:57 +0300

    JUnit Parameterized Test - Display Name

--- junit5-java-testing/src/test/java/com/vladproduction/model/OwnersParameterizedTest.java
+++ junit5-java-testing/src/test/java/com/vladproduction/model/OwnersParameterizedTest.java
@@ -1,6 +1,7 @@
 package com.vladproduction.model;
 
 import com.vladproduction.ModelsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,6 +10,13 @@ public class OwnersParameterizedTest implements ModelsTest {
     @ParameterizedTest
     @ValueSource(strings = {"Spring", "Java", "vladproduction"})
     public void testValueSource(String value){
+        System.out.println(value);
+    }
+
+    @DisplayName("Value Source Displayed Name Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Docker", "Java", "Git", "SpringBoot", "Maven", "Jenkins"})
+    public void testValueSourceDisplayedName(String value){
         System.out.println(value);
     }
 
