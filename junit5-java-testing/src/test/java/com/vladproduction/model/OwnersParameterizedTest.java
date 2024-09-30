c681719dd9153e23af6a0c3cebde1e5400e92477
vladproduction <vladbogdantsev@gmail.com>
2024-09-30 19:04:19 +0300

    JUnit Parameterized Tests - ENUM Source

--- junit5-java-testing/src/test/java/com/vladproduction/model/OwnersParameterizedTest.java
+++ junit5-java-testing/src/test/java/com/vladproduction/model/OwnersParameterizedTest.java
@@ -3,6 +3,7 @@ package com.vladproduction.model;
 import com.vladproduction.ModelsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class OwnersParameterizedTest implements ModelsTest {
@@ -19,5 +20,13 @@ public class OwnersParameterizedTest implements ModelsTest {
     public void testValueSourceDisplayedName(String value){
         System.out.println(value);
     }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    public void testEnum(OwnerType ownerType){
+        System.out.println(ownerType);
+    }
+
 
 }
