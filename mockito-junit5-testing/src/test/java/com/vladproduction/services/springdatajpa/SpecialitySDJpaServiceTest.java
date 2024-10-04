9338d6fdc22db6a69859a4412b752748200d8997
vladproduction <vladbogdantsev@gmail.com>
2024-10-04 14:45:24 +0300

    Injecting Mocks with Mockito

---
+++ mockito-junit5-testing/src/test/java/com/vladproduction/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,30 @@
+package com.vladproduction.services.springdatajpa;
+
+import com.vladproduction.model.Speciality;
+import com.vladproduction.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService specialitySDJpaService;
+
+    @Test
+    void deleteById() {
+        specialitySDJpaService.deleteById(1L);
+    }
+    @Test
+    void delete() {
+        specialitySDJpaService.delete(new Speciality());
+    }
+}
