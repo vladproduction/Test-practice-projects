7f1185a170343e136c217aed5fff673662c6af99
vladproduction <vladbogdantsev@gmail.com>
2024-10-04 15:23:21 +0300

    Verify Interactions With Mockito Mocks

--- mockito-junit5-testing/src/test/java/com/vladproduction/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ mockito-junit5-testing/src/test/java/com/vladproduction/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -22,6 +23,37 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository).deleteById(1L);
+    }
+
+    @Test
+    void deleteById_Twice() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteById_atLeastOnce() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void deleteById_atMost() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void deleteById_never() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+
+        verify(specialtyRepository, never()).deleteById(5L);
     }
     @Test
     void delete() {
