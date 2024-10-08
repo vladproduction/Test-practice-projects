31e7e507bcb212ce6f980f2b079b850b732f13e9
vladproduction <vladbogdantsev@gmail.com>
2024-10-09 00:11:43 +0300

    Spring Test Properties

---
+++ testing-spring-petclinic/src/main/java/org/springframework/samples/petclinic/vp/PropertiesWordProducer.java
@@ -0,0 +1,24 @@
+package org.springframework.samples.petclinic.vp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("external")
+@Primary
+public class PropertiesWordProducer implements WordProducer{
+
+    private String word;
+
+    @Value("${say.word}")
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String getWord() {
+        return word;
+    }
+}
