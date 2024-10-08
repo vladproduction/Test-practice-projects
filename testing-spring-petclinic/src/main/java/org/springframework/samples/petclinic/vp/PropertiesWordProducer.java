39a8cedda65146199080a4f5358282413a0b1ed6
vladproduction <vladbogdantsev@gmail.com>
2024-10-09 00:25:39 +0300

    Spring Test Properties (PropertiesLaurelTest assignment)

--- testing-spring-petclinic/src/main/java/org/springframework/samples/petclinic/vp/PropertiesWordProducer.java
+++ testing-spring-petclinic/src/main/java/org/springframework/samples/petclinic/vp/PropertiesWordProducer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("external")
+@Profile({"external", "laurel-properties"})
 @Primary
 public class PropertiesWordProducer implements WordProducer{
 
---
+++ testing-spring-petclinic/src/test/java/org/springframework/samples/petclinic/vp/junit5/PropertiesLaurelTest.java
@@ -0,0 +1,32 @@
+package org.springframework.samples.petclinic.vp.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.vp.Interpreter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource("classpath:laurel-properties.properties")
+@ActiveProfiles("laurel-properties")
+@SpringJUnitConfig(classes = PropertiesLaurelTest.TestConfig.class)
+public class PropertiesLaurelTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.vp")
+    static class TestConfig{
+    }
+    @Autowired
+    Interpreter interpreter;
+
+    @Test
+    public void whatIHear() {
+        String word = interpreter.whatIHear();
+        assertEquals("LaUreL", word);
+    }
+
+}
