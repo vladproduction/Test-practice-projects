25d0b770e7fe593b34e50408f6e8a72dc7604745
vladproduction <vladbogdantsev@gmail.com>
2024-10-23 15:15:15 +0300

    project created (as a template)

---
+++ testing-with-sb/src/main/java/com/vladproduction/testingwithsb/TestingWithSbApplication.java
@@ -0,0 +1,13 @@
+package com.vladproduction.testingwithsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWithSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWithSbApplication.class, args);
+	}
+
+}
---
+++ testing-with-sb/src/test/java/com/vladproduction/testingwithsb/TestingWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladproduction.testingwithsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWithSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
